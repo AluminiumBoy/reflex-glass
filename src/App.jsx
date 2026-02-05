f6db8ac0539bf7c3b772fd7c64a3debc6f32f77a
AluminiumBoy <cserigergo8@gmail.com>
2026-02-05 09:06:20 +0100

    v8.8.3

--- src/App.jsx
+++ src/App.jsx
@@ -1487,7 +1487,7 @@ export default function App() {
           throttleDelay = isMobile ? 800 : 200;   // mobilon kb. 1.25 fps
         } else {
           // később már kicsit gyorsabb, de még mindig nyugodt
-          throttleDelay = isMobile ? 450 : 120;
+          throttleDelay = isMobile ? 800 : 200;
         }
       } else {
         // revealing / outcome – gyorsabb, hogy a continuation jól látszódjon
