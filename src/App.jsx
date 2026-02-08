f2d0003630f52a9ac9ea2b50cf514d462efdfa2a
AluminiumBoy <cserigergo8@gmail.com>
2026-02-08 07:29:26 +0100

    v11.0.0.8.8.8.8.3

--- src/App.jsx
+++ src/App.jsx
@@ -4846,7 +4846,7 @@ export default function App() {
           src="title.png" 
           alt="REFLEX GLASS" 
           style={{ 
-            maxWidth: 620,
+            maxWidth: 800,
             width: "100%",
             height: "auto",
             marginBottom: 8
