c57adc09911e6a7a9b1510a6f91eec3108fbc2eb
AluminiumBoy <cserigergo8@gmail.com>
2026-02-08 13:16:55 +0100

    v12.0.0.8.8.8

--- src/App.jsx
+++ src/App.jsx
@@ -3372,7 +3372,7 @@ function DeveloperSupport({ onClose, onSupport }) {
         justifyContent: 'center',
         zIndex: 9999,
         padding: 20,
-        paddingTop: '5vh',
+        paddingTop: '50vh',
       }}
       onClick={onClose}
     >
