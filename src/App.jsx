14fedee2ae398d45eb6a5097567178e6ae555a81
AluminiumBoy <cserigergo8@gmail.com>
2026-02-10 16:10:38 +0100

    v13.0.0.0.8.8.8.8.8.8.7

--- src/App.jsx
+++ src/App.jsx
@@ -3800,7 +3800,7 @@ const FinalVerdict = ({ stats, onRestart, onLeaderboard, playerName }) => {
             gridTemplateColumns: "1fr 1fr 1fr",
             gap: 10,
             padding: "8 40px",
-            paddingLeft: 40,
+            paddingLeft: 15,
             marginTop: 30
           }}
         >
