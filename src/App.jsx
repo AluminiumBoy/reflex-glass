627f3720e16a8fb9e0dfdad8b5bbb2e905eba506
AluminiumBoy <cserigergo8@gmail.com>
2026-02-11 15:00:23 +0100

    v13.0.0.0.8.8.8.8.8.8.8.8.1.8.2.8.8.8.4.8.8.8.8.7

--- src/App.jsx
+++ src/App.jsx
@@ -5521,8 +5521,8 @@ export default function App() {
         style={{
           position: "fixed",
           bottom: 30,
-          left: 30,
-          zIndex: 100,
+          left: 15,
+          zIndex: 200,
           display: "flex",
           flexDirection: "column-reverse",
           alignItems: "center",
